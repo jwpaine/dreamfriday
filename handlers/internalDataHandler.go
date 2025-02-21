0d2977a4c17d99d384702ccd07e195b4dc9c1a5a
tsunami <11170625+jwpaine@users.noreply.github.com>
2025-02-21 14:25:31 -0500

    comments

--- handlers/internalDataHandler.go
+++ handlers/internalDataHandler.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	pageengine "dreamfriday/pageengine"
 	"fmt"
-	"log"
 
 	"github.com/labstack/echo/v4"
 )
@@ -19,7 +18,6 @@ func RouteInternal(path string, c echo.Context) (interface{}, error) {
 		}
 		// Return only the "sites" element from userDataMap
 		if cachedSitesElement, exists := userData["sites"].(pageengine.PageElement); exists {
-			log.Println("Serving cached sites for user")
 			return cachedSitesElement, nil
 		}
 		return nil, fmt.Errorf("sites not found in user data")
@@ -29,9 +27,8 @@ func RouteInternal(path string, c echo.Context) (interface{}, error) {
 		if err != nil {
 			return nil, err
 		}
-		// Return only the "sites" element from userDataMap
+		// Return only the "handle" element from userDataMap
 		if cachedHandleElement, exists := userData["handle"].(pageengine.PageElement); exists {
-			log.Println("Serving cached sites for user")
 			return cachedHandleElement, nil
 		}
 		return nil, fmt.Errorf("address not found in user data")
